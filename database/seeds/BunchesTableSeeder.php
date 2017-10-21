3e83b9043232332be1448fb7dd7e61340ff828d2
Sergey Okhrymenko <mr.inceon@gmail.com>
2017-10-21 15:28:09 +0300

    added seeds for db tables

---
+++ database/seeds/BunchesTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BunchesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('bunches')->insert([
+            'name' => 'first',
+            'status' => 1,
+            'description' => 'My first bunch',
+            'created_by' => 1,
+            'updated_by' => 1,
+        ]);
+        DB::table('bunches')->insert([
+            'name' => 'second',
+            'status' => 1,
+            'description' => 'Not my second bunch',
+            'created_by' => 2,
+            'updated_by' => 2,
+        ]);
+        DB::table('bunches')->insert([
+            'name' => 'third',
+            'status' => 1,
+            'description' => 'My third bunch',
+            'created_by' => 1,
+            'updated_by' => 1,
+        ]);
+    }
+}
