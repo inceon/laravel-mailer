74ba4e6b1572ad5dfd767a395e98b33611c47b29
Sergey Okhrymenko <mr.inceon@gmail.com>
2017-10-21 15:29:57 +0300

    small changes in view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        Test task using the tools of the Laravel 5 framework, which, according to a certain list of emails, will send out the selected template with the html-code according to the pre-prepared list of e-mails....
+    </div>
+@endsection
